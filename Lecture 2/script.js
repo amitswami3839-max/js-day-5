0e33d57fb68cf6ce12345408b5a0462057f7dda5
amitswami3839-max <amitswami3839@gmail.com>
2026-01-23 12:41:30 +0530

    sesssion note

---
+++ Lecture 2/script.js
@@ -0,0 +1,172 @@
+// // call=demo 
+// // function sample(call){
+// //     console.log("sample");
+// //     call();
+// // }
+// // function demo(){
+// //     console.log("demo")
+// // }
+// // sample(demo)
+
+// // let numbers=[1,2,3]
+// // let doubled=numbers.map((x)=>x)
+// // console.log(doubled);
+
+// // let marks=[10,40,30,20,34,29,0.5,16,8,1]
+
+// // let result= marks.filter((mark)=>mark>=16);
+// // console.log(result);
+
+// // question - take different marks and print the marks and message
+// // of pass and fail for example-"pass"
+
+
+// // let marks=[10,20,30,22,24,18,17,15,12,14]
+// // let result= marks.filter((mark)=>mark>=12)
+// // marks.forEach(mark=>{
+// //     if (mark >= 12){
+// //         console.log(mark + "pass");
+// // }else{
+// //     console.log(mark + "fail");
+// // }
+// // });
+// // let fruits = ["apple,"banana];
+// // let newlength = fruits.push("cherry");
+// //pop
+// // let fruits = ['apple','banana','cherry]
+// // Q1. reverse the Array using Push and pop method
+// let fruit =['apple', 'banana', 'cherry']
+// fruit.pop()
+// fruit.pop()
+// fruit.pop()
+// fruit.push('cherry')
+// fruit.push('banana')
+// fruit.push('apple')
+
+// console.log(fruit)
+
+// // Q2 remove all negative Number forme an Array
+// let arr=[5,-3,16,-11,-7,8]
+// let result=[]
+
+// for(let i=0;i<arr.length;i++)
+//     if(arr[i]>=0){
+//         result.push(arr[i]);
+//     }
+
+//  console.log(result)
+// // remove all negative number frome an arry 
+// // let arr=[5,-3,16,-11,-7,8]
+// // let result=[]
+// // whike(number.length > 1){
+//     //   number.pop()
+// // }
+// // console.log(number)
+// while(arr.length>0){
+//     let val = arr.shift();
+//     if(val>0){
+//         result.push(val);
+//     }         
+// }
+// CSSViewTransitionRule.log(res);
+// // Q3. check if arry is palindrome or not.
+// let arr2=[1,2,3,2,1]
+
+// let orignal =[];
+
+// for(let i=0;i<arr.length;i++){
+//     orignal.push(arr2[i]);
+//     console.log(orignal);
+// }
+// while(arr2.length > 0){
+//     Reverse.push(arr.pop())
+// }
+// for(i=0;i<orignal.length;i++){
+//     if(orignal{i} !== Reverse[i]){
+//         retune "not a palindrome"
+        
+//     }
+//   retune "its a palindrom";  
+// }
+// console.log(palindrome());
+
+
+// //retune statement the block and comes out of the block
+// //smile the code exceuationhits the retune statement, it will directaly eradicate the execuation of the funcation
+// //  and retune the value specified
+let sentence = "I am a comma separated sentence";
+let word = sentence.split(" ");
+console.log(word);//[ 'I' , 'm','comm','sep
+
+
+
+
+
+
+
+
+
+
+
+// find out the occcurance of "r" in thne given text
+let str="tu meri me tera me tera tu meri";
+
+// answer
+
+let count = 0;
+for(let i = 0; i < str.length; i++) {
+    if (str[i]===){
+        count++;
+    }
+}
+
+console.log("occurrence of 'r':",count);
+
+
+
+
+
+
+
+// function sample(){
+//}
+
+// callback=demo()
+function sample(callback){
+    console.log("demo")
+}
+sample(demo)
+
+//arrow function
+function demo3
+const demo=()=>console.log("demo");
+demo()
+
+
+
+// firstclass function
+//1 we can treat a variable as a function
+//2. we can pass a function as an argument inside another function
+//3. we can return a function from a function
+
+function test(){
+    return function test2(){
+        console.log("test2"):
+        return function test3(){
+            console.log("test3")
+        }
+    }
+}
+//test()()()
+let value = test()
+let value2=value()
+value2(20)
+
+//4.higher order function
+
+//map, filter,reduce
+
+setTimeout((=>)100
+){
+
+},
